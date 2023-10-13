155a4d515fde2a201f09261fb10f328f38aef02e
Krystian Rodrigo <zS21004519@estudiantes.uv.mx>
2023-10-12 20:30:19 -0600

    correcciones

--- index.js
+++ index.js
@@ -9,29 +9,17 @@ app.options('*', cors());
 const port = 8080;
 
 app.get('/', (req, res, next) => {
-	res.send('node express api <br> by adsoft');
+	res.send('node express api <br> by Rodrigo');
 });
 
 
-app.get('/header', (req, res, next) => {
-  res.sendfile("assets/header.json");
+app.get('/1a_proxy_size', (req, res, next) => {
+  res.sendfile("assets/1a_proxy_size.json");
 });
 
-app.get('/work-experience', (req, res, next) => {
-	res.sendfile("assets/work-experience.json");
+app.get('/1a_dev_hours', (req, res, next) => {
+	res.sendfile("assets/1a_dev_hours.json");
   });
-
-app.get('/skills', (req, res, next) => {
-	res.sendfile("assets/skills.json");
-  });
-
-  app.get('/certificates', (req, res, next) => {
-	res.sendfile("assets/certificates.json");
-  });
-
-  app.get('/interests', (req, res, next) => {
-    res.sendfile("assets/interests.json");
-    });
 
 app.listen(port,  () => 
 	console.log('listening on port ' + port
