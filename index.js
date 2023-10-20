95a16db373509d9dd8276eaab310a6c2a274ffac
Krystian Rodrigo <zS21004519@estudiantes.uv.mx>
2023-10-20 09:44:38 -0600

    test reto 2

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.options('*', cors());
 
 const port = 8080;
 
+const Data_Test1 = require ('./assets/3A_TEST1.json');
+const Data_Test2 = require ('./assets/3A_TEST2.json');
+const Data_Test3 = require ('./assets/3A_TEST3.json');
+const Data_Test4 = require ('./assets/3A_TEST4.json');
+
 app.get('/', (req, res, next) => {
 	res.send('node express api <br> by Rodrigo');
 });
@@ -19,6 +24,22 @@ app.get('/1a_proxy_size', (req, res, next) => {
 
 app.get('/1a_dev_hours', (req, res, next) => {
 	res.sendfile("assets/1a_dev_hours.json");
+  });
+
+app.get('/3A_TEST1', (req, res) => {
+	res.json(Data_Test1);
+  });
+
+app.get('/3A_TEST2', (req, res) => {
+	res.json("Data_Test2.json");
+  });
+
+app.get('/3A_TEST3', (req, res) => {
+	res.json(Data_Test3);
+  });
+
+app.get('/3A_TEST4', (req, res) => {
+	res.json(Data_Test4);
   });
 
 app.listen(port,  () => 
